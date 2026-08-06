581a113aac9ffa31eeb2cb3bf25e8d758eba1f0a
MHowell615 <mhowell615@gmail.com>
2026-08-06 17:03:36 +0200

    fix BillingManager.ios.kt

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "cloud.scoreprof.app"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 53
-        versionName = "2.1.27"
+        versionCode = 54
+        versionName = "2.1.28"
     }
     packaging {
         resources {
